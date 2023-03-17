83a1cfa1f06329ac00ff6f688fcc7333cca68f45
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-16 19:10:40 -0700

    added CRD functionality to friend routtes

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,52 +1,54 @@
-const express = require('express');
-const Friend = require('../models/friend');
-const User = require('../models/user');
-const Note = require('../models/note');
+const express = require('express')
+const Friend = require('../models/friend')
+const User = require('../models/user')
+const Note = require('../models/note')
 
-const router = express.Router();
+const router = express.Router()
 
 // Get all friends
-router.get('/', async (req, res) => {
-  try {
-    Friend.findAll({
-      include: [{ model: User }, { model: Note }],
-    });
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+router.get('/', (req, res) => {
+  Friend.findAll({
+  })
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({
+        msg: 'Something went wrong',
+        err,
+      })
+    })
+})
 
 // Get a single friend
 router.get('/:id', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   try {
     const friend = await Friend.findByPk(id, {
-      include: [{ model: User }, { model: Note }],
-    });
+      
+    })
     if (!friend) {
-      res.status(404).json({ message: 'Friend not found' });
+      res.status(404).json({ message: 'Friend not found' })
     } else {
-      res.json(friend);
+      res.json(friend)
     }
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    console.error(err)
+    res.status(500).json({ message: 'Server error' })
   }
-});
+})
 
 // Create a friend
 router.post('/', async (req, res) => {
-  const { name, userId } = req.body;
+  const { username, userId } = req.body;
   try {
-    const user = await User.findByPk(userId);
-    if (!user) {
-      res.status(400).json({ message: 'User not found' });
-    } else {
-      const friend = await Friend.create({ name, UserId: userId });
-      res.json(friend);
+    if (!username) {
+      throw new Error('Username is required');
     }
+
+    const friend = await Friend.create({ username, userId });
+    res.json(friend);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
@@ -55,38 +57,38 @@ router.post('/', async (req, res) => {
 
 // Update a friend
 router.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
+  const { id } = req.params
+  const { name } = req.body
   try {
-    const friend = await Friend.findByPk(id);
+    const friend = await Friend.findByPk(id)
     if (!friend) {
-      res.status(404).json({ message: 'Friend not found' });
+      res.status(404).json({ message: 'Friend not found' })
     } else {
-      friend.name = name;
-      await friend.save();
-      res.json(friend);
+      friend.name = name
+      await friend.save()
+      res.json(friend)
     }
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    console.error(err)
+    res.status(500).json({ message: 'Server error' })
   }
-});
+})
 
 // Delete a friend
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   try {
-    const friend = await Friend.findByPk(id);
+    const friend = await Friend.findByPk(id)
     if (!friend) {
-      res.status(404).json({ message: 'Friend not found' });
+      res.status(404).json({ message: 'Friend not found' })
     } else {
-      await friend.destroy();
-      res.json({ message: 'Friend deleted successfully' });
+      await friend.destroy()
+      res.json({ message: 'Friend deleted successfully' })
     }
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    console.error(err)
+    res.status(500).json({ message: 'Server error' })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
